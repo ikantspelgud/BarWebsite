88aa86459dbb85f9b7e612b7e0fa16071e69effe
Kathleen Garitty <katgaritty@gmail.com>
2017-06-19 13:17:40 -0500

    updated file structure with new components folder; added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import NavigationBar from './components/NavigationBar';
+import Home from './components/Home';
+import Drinks from './components/Drinks';
+import Directions from './components/Directions';
+import Musicians from './components/Musicians';
+import Schedule from './components/Schedule';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <NavigationBar/>
+          <Switch>
+            <Route path="/drinks" component={Drinks}/>
+            <Route path="/location" component={Directions}/>
+            <Route path="/musicians" component={Musicians}/>
+            <Route path="/schedule" component={Schedule}/>
+            <Route path="/" component={Home}/>
+          </Switch>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+
+export default () => {
+  return (
+    <Navbar>
+      <Navbar.Header>
+        <LinkContainer to="/">
+          <Navbar.Brand>Donn's Depot</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <LinkContainer to="/schedule">
+            <NavItem eventKey={1}>Schedule</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/musicians">
+            <NavItem eventKey={2}>Musicians</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/drinks">
+            <NavItem eventKey={3}>Drinks</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/location">
+            <NavItem eventKey={4}>Location</NavItem>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
