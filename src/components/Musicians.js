d7d12bb8d45d99a760730999c3262e4d7dc0983d
Tom <ikantspelgud@gmail.com>
2017-06-19 19:16:04 -0500

    Beginning of adding spotify embedded players using iframes

--- src/components/Musicians.js
+++ src/components/Musicians.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Iframe from 'react-iframe'
 
 export default (props) => {
   return (
-    <div>Musicians</div>
+    <div>
+      Albert & Gage
+      <SpotifyPlayer
+        src="https://embed.spotify.com/?uri=spotify:user:1247199566:playlist:3IZnq8jKmvBOXDbGtaWMRM"
+      />
+    </div>
   );
-};
+}
+
+
+class SpotifyPlayer extends Component {
+
+  render() {
+    return (
+      <Iframe 
+        url={this.props.src}
+        width="300px"
+        height="380px"
+        frameborder="0"
+        allowtransparency="true"
+      />
+    )
+  }
+}
